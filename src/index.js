2e964a204d3b752959c340dead87a31b6b289f67
Marcelo <marcelo.mfilho2804@gmail.com>
2022-03-02 15:05:31 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(json());
+
+server.listen(4000);
