63eef3f149e0c7b3b6a09301e290f98b81d094ef
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-03-14 12:42:37 -0300

    Preparando revisão de código

--- src/routes/cotegoriesRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getCategories, insertCategory } from "../controllers/categoriesControler.js";
-
-const categoriesRouter = Router();
-
-categoriesRouter.post('/categories', insertCategory)
-categoriesRouter.get('/categories', getCategories)
-
-export default categoriesRouter;
